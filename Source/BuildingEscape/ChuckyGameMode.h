8b3a82217fc297e056f7e2693336ed9f5da132bb
Charles A Edwards <charles.a.edwards@btinternet.com>
2018-05-21 15:34:48 +0100

    Default pawn now has the grabber properly attached to it

---
+++ Source/BuildingEscape/ChuckyGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BuildingEscapeGameModeBase.h"
+#include "ChuckyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_API AChuckyGameMode : public ABuildingEscapeGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Grabber.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -20,6 +21,7 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
 	
 }
 
